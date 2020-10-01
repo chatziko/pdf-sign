5bc4f7d18a2102697823aed210d683f1685034a6
Kostas Chatzikokolakis <kostas@chatzi.org>
2020-10-01 17:09:28 +0300

    improve pdf-sign.sh

--- scripts/pdf-sign.sh
+++ scripts/pdf-sign.sh
@@ -3,6 +3,13 @@
 # Simple GUI using zenity, useful for nautilus integration
 
 PIN=`zenity --entry --title="Enter your PIN" --text="PIN:" --hide-text`
-RES=`pdf-sign.py $1 --pin="$PIN"`
+RES=`pdf-sign.py $1 --pin="$PIN" 2>&1`
 
-zenity --info --title="pdf-sign" --text="$RES"
+if [ $? -eq 0 ]
+then
+	DIALOG=--info
+else
+	DIALOG=--error
+fi
+
+zenity $DIALOG --title="pdf-sign" --text="$RES" --width=300
